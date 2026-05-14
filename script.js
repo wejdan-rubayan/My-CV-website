0c44aaf5e84ee0e308a54b7961c93c53c504bfa7
Wejdan bin Rubayan <wejdanbinrubayan@gmail.com>
2026-05-14 10:52:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('githubBtn');
+
+btn.addEventListener('click', function() {
+    alert('Redirecting to Wejdan\'s GitHub profile.');
+    window.open('https://github.com/wejdan-rubayan', '_blank');
+});
